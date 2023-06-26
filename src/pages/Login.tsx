1b63faab90de2108109168c9f8e40dcce69d06ac
mzajaczkowsky <mzajaczkowsky@gmail.com>
2023-06-26 22:19:35 +0200

    redirect for logo in login and register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import logo from "../assets/gravetracelogo.svg"
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoginField from "../components/LoginField";
 
 
@@ -7,13 +7,13 @@ export default function Login() {
     return (
         <div className="login">
             <div className="navbar">
-                    <div className="navbar__logo">
-                        <img src={logo} alt="GraveTrace" />
-                    </div>
+                <div className="navbar__logo">
+                    <Link to="/"><img src={logo} alt="GraveTrace" /></Link>
+                </div>
             </div>
             <div className="login__form">
                 <LoginField />
             </div>
         </div>
-        );
+    );
 }
