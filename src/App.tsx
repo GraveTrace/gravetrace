aa96499bfcf265cacb2cfc05447c847fd15b06a1
mzajaczkowsky <mzajaczkowsky@gmail.com>
2023-06-26 22:20:02 +0200

    new route for creating new dead profile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import CreateDeadProfile from "./pages/CreateDeadProfile";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<CreateDeadProfile />} />
       </Routes>
     </Router>
   )
